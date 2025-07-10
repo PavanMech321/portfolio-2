498d3ed55226a59e185d9c582a4946fd9aa28453
PavanMech321 <durgapavan54321@gmail.com>
2025-07-10 23:44:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <div class="footer">
+      <a href="#Home" style="color:white; background-color:blue; width:fit-content; border-radius:10px;text-align:right;height:50px;margin-top:20px;">Go to Top</a>
+    </div>
   </StrictMode>,
 )
